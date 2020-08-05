63c3f4b4eb4afa7f64a769bc4020a56b28ca4e86
Sambo Chea <sombochea@cubetiqs.com>
2020-08-05 20:14:11 +0700

    Fixed webpack config and add replace to mini extractor back

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,80 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+// load the mini csss extractor
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    modules: [path.join(__dirname, "src"), "node_modules"],
+    alias: {
+      react: path.join(__dirname, "node_modules", "react"),
     },
-    resolve: {
-        modules: [path.join(__dirname, 'src'), 'node_modules'],
-        alias: {
-        react: path.join(__dirname, 'node_modules', 'react'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-    },
-    module: {
-        rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-            loader: 'babel-loader',
-            },
-        },
-        {
-            test: /\.s?css$/,
+      },
+      {
+        // test: /\.s?css$/,
+        test: /\.s[ac]ss$/i,
 
-            // use: [
-            //     {
-            //         loader: 'style-loader',
-            //     },
-            //     {
-            //         loader: 'css-loader',
-            //     },
-            // ],
+        // use: [
+        //     {
+        //         loader: 'style-loader',
+        //     },
+        //     {
+        //         loader: 'css-loader',
+        //     },
+        // ],
 
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
-                use: [
-                    "css-loader", 
-                    "sass-loader"
-                ]
-            })
-        },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html',
-        }),
-        new ExtractTextPlugin("styles.css"),
+        // use: ExtractTextPlugin.extract({
+        //     fallback: "style-loader",
+        //     use: [
+        //         "css-loader",
+        //         "sass-loader"
+        //     ]
+        // }),
+
+        /** This use for split the file of style */
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        
+        /** This use for signle bundle file */
+        // use: [
+        //   // fallback to style-loader in development
+        //   process.env.NODE_ENV !== "production"
+        //     ? "style-loader"
+        //     : MiniCssExtractPlugin.loader,
+        //   "css-loader",
+        //   "sass-loader",
+        // ],
+
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+    
+    // this use for load the plugin
+    new MiniCssExtractPlugin(),
+
+    //new ExtractTextPlugin("styles.css"),
+    // new ExtractTextPlugin({
+    //   filename: "bundle.css",
+    //   disable: false,
+    //   allChunks: true,
+    // }),
+  ],
 };
